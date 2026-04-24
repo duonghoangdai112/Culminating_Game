0dce29a86a1c58c59ae02106c42d993b9c5861e1
Mark H <60531244+duonghoangdai112@users.noreply.github.com>
2026-04-24 14:59:03 -0400

    Update projectile, tiles and monster
    This is only framework .monster need to work more on.

--- Culminating_Game/src/Monster.java
+++ Culminating_Game/src/Monster.java
@@ -1,4 +1,96 @@
+import java.util.*;
 
-public class Monster {
+public abstract class Monster {
+	
+	private double health; 
+	private double damgage; 
+	private double visionRange; 
+	private double speedX;
+	private double speedY;
+	private double speed;
+	private double cooldown; 
+	private int x; 
+	private int y; 
+	private ArrayList<Projectile> projectiles; 
+	private double hitBox; 
+	private int startTime; 
+	//image
+	
+	// initialize all attribute here 
+	Monster(){
+		speed =  Math.sqrt(speedX*speedX +speedY *speedY);
+	}
+	
+	public double getHealth() {
+		return health;
+	}
+	
+	public abstract void Attack() ;
+	//when create a bullet just get the monster speedX and Y and times it by cons for the speed
+	
+	/**
+	 * Move the position of the monster toward character
+	 * @param dirX indicate x direction (1 right, 0 stationary , 2 left)
+	 * @param dirY indicate y direction (1 up , 0 stationary , -1 down) 
+	 */
+	public void move() {
+		x += speedX;
+		y += speedY;
+		
+		//display image 
+	}
+	/**
+	 * Turn and move the monster toward the character 
+	 * @param charX character X position
+	 * @param charY character Y position
+	 */
+	public void pathFinding(int charX, int charY) {
+		int deltX = x- charX; 
+		int deltY = y - charY; 
+		double angelY = Math.atan(deltY/deltX);
+		double angelX = 90-angelY;
+		
+		speedX = speed *Math.sin(angelX);
+		speedY = speed* Math.sin(angelY);
 
+		this.move();
+		
+	} 
+	/**
+	 * update bullet position and call the monster to attack if ready
+	 * @param time the time from the game clock
+	 */
+	public void checkCD(int time ) {
+		// if (time-startTime) % cooldown == 0 => this.attack()
+		// for each projectile => p.move() 
+	}
+	
+	/**
+	 * Check if monster projectile hit block or character and do action accordingly
+	 * @param charX - character X position
+	 * @param charY - character Y position 
+	 * @param Rtiles - collection of room tiles
+	 * @return
+	 */
+	public double checkProjectile (int charX, int charY, ArrayList<Tiles> Rtiles) {
+		double monDamage = 0;
+		for(Projectile p : projectiles){
+			if(this.x == charX && this.y == charY) {
+				//make bullet invisible
+				// damage = p.damage
+			}
+			for(Tiles t : Rtiles) {
+				// if (this.x == t.getX() && this.y == t.getY()){
+				//		if(blockable) => delete projectile 
+				// }
+			}
+		}
+		
+		return monDamage;
+	}
+	
+	// draw method 
+	
+	
+	
 }
